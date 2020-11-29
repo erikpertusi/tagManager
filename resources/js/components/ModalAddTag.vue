daba706fc74948720d14bf4bd58e7e31f5035b14
Erik Pertusi <erik.pertusi@agv.com.br>
2020-11-29 09:35:10 -0300

    Associando Tag ao Repositorio

--- resources/js/components/ModalAddTag.vue
+++ resources/js/components/ModalAddTag.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="modal" tabindex="-1" aria-labelledby="" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -12,16 +12,12 @@
                     <div class="row">
                         <div class="col-sm-12">
                             <ul class="list-group list-group-horizontal-sm">
-                                <li v-for="item in tags" :key="item.id" class="list-group-item" style="cursor:pointer;" v-on:click="" title="Adicionar Tag">
+                                <li v-for="item in tags" :key="item.id" class="list-group-item" style="cursor:pointer;" v-on:click="storeRepository(item.id)" title="Adicionar Tag">
                                     <i class="fa fa-tag text-success" aria-hidden="true"></i> <b>{{ item.name }}</b>
                                 </li>
                             </ul>
                         </div>
                     </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
                 </div>
             </div>
         </div>
@@ -30,24 +26,45 @@
 
 <script>
     export default {
+        props: {
+          item: {}
+        },
         data() {
             return {
                 tags: [],
-                nameTag: null,
-                idTag: 0,
-                page: 1
+                page: 1,
+                perPage: 50,
+                total: 0,
+                next: false,
+                idRepository: 0,
             }
         },
         methods: {
-            loadTag(item) {
-                this.nameTag = item.name;
-                this.idTag = item.id;
-            },
-            storeTag() {
+            storeRepository(idTag) {
+                let item = this.$props.item;
+                let data = {
+                    name: item.name,
+                    owner: item.owner.login,
+                    link: item.html_url,
+                    language: item.language,
+                    stars: item.watchers,
+                };
                 let self = this;
-                axios.post('/tags', {name: this.nameTag, id: this.idTag})
+                axios.post('/repositories', data)
                     .then(function (response) {
-                        self.getTags();
+                        self.idRepository = response.data.id;
+                        self.setTagToRepository(idTag)
+
+                    });
+            },
+            setTagToRepository(idTag) {
+                let data = {
+                    idTag: idTag,
+                    idRepository: this.idRepository
+                };
+                axios.post('/tagRepository', data)
+                    .then(function (response) {
+                        $('#modal').modal('toggle');
                     });
             },
             getTags(next, previous) {
@@ -57,7 +74,11 @@
                 if (previous) {
                     this.page--;
                 }
-                axios.get('./api/tags?p=' + this.page).then(response => this.tags = response.data);
+                let self = this;
+                axios.get('./api/tags?p=' + this.page + '&perPage=' + this.perPage).then((response) => {
+                    self.tags = response.data.items;
+                    self.total = response.data.total_count;
+                });
             }
         },
         mounted() {
